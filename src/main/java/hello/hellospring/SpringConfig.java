32df11c089121cf0b2ef3ffde302a4ee4a3d2d36
iedcon <iedcon12@gmail.com>
2021-06-26 23:23:06 +0900

    AOP 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -32,6 +32,7 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+
 //    @Bean
 //    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
